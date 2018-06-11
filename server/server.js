0b5170e74f8b184d940a026deef200c964149077
Ash Thwaites <ash@trypitch.com>
2018-06-11 20:27:35 +0100

    emit messages

--- server/server.js
+++ server/server.js
@@ -2,15 +2,45 @@ require("./config/config");
 
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const bodyParser = require("body-parser");
 
 const publicPath = path.join(__dirname, "../public");
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
 const port = process.env.PORT;
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    // socket.emit("newMessage", {
+    //     from: "from message",
+    //     body: "message body",
+    //     createdAt:123
+    // });
+
+    socket.on("createMessage", (data) =>{
+        console.log("createMessage", data);
+        io.emit("newMessage",{
+            from: data.from,
+            text: data.text,
+            createdAt: new Date().getTime()
+        })
+    });
+
+    socket.on("disconnect", () =>{
+        console.log("User disconnected");
+    });
+});
+
+
+server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
 
